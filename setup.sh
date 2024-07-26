6cec9514043f469bb0d88fc3cab41c64963d9e8f
shubhamkode <shubhamkode@gmail.com>
2024-07-26 21:58:20 +0530

    v0.0.5
    Cleanup

--- setup.sh
+++
@@ -1,5 +0,0 @@
-setxkbmap -layout 'us' -variant 'dvorak'
-sudo apt install git
-cd && git clone https://github.com/shubhamkode/.dotfiles.git
-sudo chmod +x ./.dotfiles/setup.py
-./.dotfiles/setup.py
